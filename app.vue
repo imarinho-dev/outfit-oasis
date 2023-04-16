e1e2c30be49131b861a7752d96640e97914689f4
imarinho-dev <imarinho.dev@gmail.com>
2023-04-16 15:50:55 -0300

    navegation component added

--- app.vue
+++ app.vue
@@ -1,18 +1,18 @@
 <template>
-	<q-layout view="hHh lpR fFf">
-		<q-header reveal elevated class="text-white bg-dark">
-			<!-- <nav-bar /> -->
-		</q-header>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <navigation-bar />
+    </q-header>
 
-		<q-page-container>
-			<NuxtPage />
-		</q-page-container>
+    <q-page-container>
+      <NuxtPage class="pt-[25px]" />
+    </q-page-container>
 
-		<q-footer elevated>
-			<!-- <Footer /> -->
-		</q-footer>
-	</q-layout>
+    <q-footer elevated>
+      <!-- <Footer /> -->
+    </q-footer>
+  </q-layout>
 </template>
 <script setup lang="ts">
-import '~/assets/css/main.css';
+import "~/assets/css/main.css";
 </script>
---
+++ components/NavigationBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-toolbar
+    reveal
+    class="navigation flex h-[70px] justify-between bg-white uppercase text-gray-600"
+    ><q-toolbar-title shrink class="logo-container p-0">
+      <NuxtLink class="text-xl font-semibold tracking-wider" to="/"
+        >outfit oasis</NuxtLink
+      >
+    </q-toolbar-title>
+    <div class="nav-links-container flex w-1/2 items-center justify-end">
+      <NuxtLink class="nav-link px-[15px]" to="/about">About</NuxtLink>
+    </div>
+  </q-toolbar>
+</template>
